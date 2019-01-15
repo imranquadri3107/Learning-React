bcc00f8499b01fd94436a798d6b73439c0a8ce8c
imranquadri3107 <imranquadri77@gmail.com>
2019-01-14 19:58:37 -0800

    Updated

--- src/App.js
+++ src/App.js
@@ -4,13 +4,26 @@ import Form from "./components/Form";
 
 class App extends Component {
 
-  getRecipe =(e)=>{
-    e.preventDefault()
-    console.log('working!!!')
+  state = {
+    recipes: []
   }
 
+  
+  // https://www.food2fork.com/api/search?key=2930981db6150663e713632187ef62bc&q=shredded%20chicken
 
+  getRecipe =async(e)=>{
 
+    const API_KEY = '2930981db6150663e713632187ef62bc';
+    const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault()
+    const api_call =await fetch(`https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=shredded%20chicken&count=10`);
+  
+    const data = await api_call.json();
+    this.setState({recipes: data.recipes})
+    console.log(this.state.recipes);
+  }
+
+  
   render() {
     return (
       <div className="App">
--- src/components/Form.js
+++ src/components/Form.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Form =(porps)=> (
     <form onSubmit ={porps.getRecipe}>
-        <input type =  'text'/>
+        <input type =  'text' name = 'recipeName'/>
         <button>Search</button>
     </form>
 );
