4e8f5254e713b669a4a55b309b7df30560d074ba
imranquadri3107 <imranquadri77@gmail.com>
2019-01-20 19:30:46 -0800

    Updated

--- src/App.js
+++ src/App.js
@@ -24,6 +24,19 @@ class App extends Component {
     console.log(this.state.recipes);
   }
 
+  componentDidMount=()=>{
+
+    const json = localStorage.getItem('recipes');
+    const recipes = JSON.parse(json);
+    this.setState({recipes});
+
+
+  }
+
+  componentDidUpdate = ()=> {
+    const recipes = JSON.stringify(this.state.recipes);
+    localStorage.setItem("recipes", recipes);
+  }
   
   render() {
     return (
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link } from "react-router-dom";
 
 const API_KEY = '2930981db6150663e713632187ef62bc';
 
@@ -13,13 +14,33 @@ class Recipe extends React.Component{
         const req = await fetch(`https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=${title}`);
       
         const res = await req.json();
-        console.log(res.recipes[0]);
+        this.setState({ activeRecipe: res.recipes[0] });
+        console.log(this.state.activeRecipe);
         
     }
     render(){
-        console.log(this.props);
+        const recipe = this.state.activeRecipe;
         return(
-            <div>Recipe componenet</div>
+            <div className = 'container'>
+                {
+                    this.state.activeRecipe.length !== 0 &&
+                    <div className='active-recipe'>
+                        <img className='active-recipe__img' src={recipe.image_url}
+                            alt={recipe.title} />
+                        <h3 className='active-recipe__title'>{recipe.title}</h3>
+                        <h4 className='active-recipe__publisher'>
+                            Publisher: <span>{recipe.publisher}</span>
+                        </h4>
+
+                        <p className="active-recipe__website">
+                            Website: <span><a href={recipe.publisher_url}>{recipe.publisher_url}</a></span>
+                        </p>
+                        <button className='active-recipe__button'>
+                        <Link to="/">Go Home</Link>
+                        </button>
+                    </div>
+                }
+            </div>
         );
     }
 }
