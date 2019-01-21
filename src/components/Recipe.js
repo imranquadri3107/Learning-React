a1a9b109e5634509723b7ddee0f62413d0084abf
imranquadri3107 <imranquadri77@gmail.com>
2019-01-20 18:49:27 -0800

    Updated

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,6 +1,21 @@
 import React from 'react';
 
+const API_KEY = '2930981db6150663e713632187ef62bc';
+
 class Recipe extends React.Component{
+    state = {
+        activeRecipe: []
+    }
+
+    componentDidMount =async ()=>{
+
+        const title = this.props.location.state.recipe;
+        const req = await fetch(`https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=${title}`);
+      
+        const res = await req.json();
+        console.log(res.recipes[0]);
+        
+    }
     render(){
         console.log(this.props);
         return(
--- src/components/Router.js
+++ src/components/Router.js
@@ -8,7 +8,7 @@ const Router = ()=> (
     <BrowserRouter>
         <Switch>
         <Route path = "/" component={App} exact/>
-        <Route path = "/Recipe/:id" component={Recipe}/>
+        <Route path = "/recipe/:id" component={Recipe}/>
         </Switch>
     </BrowserRouter>
 );
